021f46db8d5342feb5ffe0c4b243b627b6e80b3e
Anh Tuan <anhtuan9702@gmail.com>
2024-06-06 08:56:19 +0700

    Debezium is running now, dont know why

--- fosho/src/main/java/com/vdt/fosho/config/DebeziumConfig.java
+++ fosho/src/main/java/com/vdt/fosho/config/DebeziumConfig.java
@@ -16,23 +16,37 @@ public class DebeziumConfig {
     @Bean
     public io.debezium.config.Configuration customerConnector() {
         return io.debezium.config.Configuration.create()
+
+                // Debezium properties
                 .with("name", "custom-mariadb-connector")
                 .with("connector.class", "io.debezium.connector.mysql.MySqlConnector")
                 .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", "/tmp/offsets.dat")
+                .with("offset.storage.file.filename", "F:\\Debezium/offsets.dat")
                 .with("offset.flush.interval.ms", 60000)
+
+                // MariaDB properties
+                .with("connector.adapter", "mariadb")
+                .with("database.protocol", "jdbc:mariadb")
+                .with("database.jdbc.driver", "org.mariadb.jdbc.Driver")
                 .with("database.hostname", "localhost")
                 .with("database.port", 3306)
                 .with("database.user", "root")
                 .with("database.password", "password")
                 .with("database.dbname", "fosho")
-                .with("topic.prefix", "fosho.vdt")
-                .with("database.include.list", "fosho")
+                .with("database.ssl.mode", "disabled")
+                .with("table.include.list", "fosho.restaurants,fosho.orders,fosho.dishes")
+
+                // ???
                 .with("include.schema.changes", "false")
-                .with("database.server.id", 85744)
-                .with("database.server.name", "custom-mariadb-connector")
+                .with("database.server.id", 6000)
+                .with("database.server.name", "fosho-connector")
                 .with("database.history", "io.debezium.relational.history.FileDatabaseHistory")
-                .with("database.history.file.filename", "/tmp/dbhistory.dat")
+                .with("database.history.file.filename", "F:\\Debezium/dbhistory.dat")
+
+                // Kafka
+                .with("topic.prefix", "vdt")
+                .with("schema.history.internal", "io.debezium.storage.file.history.FileSchemaHistory")
+                .with("schema.history.internal.file.filename", "F:\\Debezium/history.dat")
                 .build();
     }
 }
--- fosho/src/main/java/com/vdt/fosho/config/DebeziumListener.java
+++ fosho/src/main/java/com/vdt/fosho/config/DebeziumListener.java
@@ -9,10 +9,12 @@ import io.debezium.engine.format.ChangeEventFormat;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.Data;
+
 import org.apache.kafka.connect.source.SourceRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,46 +22,37 @@ import java.util.concurrent.Executors;
 @Data
 public class DebeziumListener {
 
-    private final Configuration customerConnectorConfiguration;
+    private final Configuration connectorConfiguration;
     private final RestaurantService restaurantService;
+
     private DebeziumEngine<RecordChangeEvent<SourceRecord>> debeziumEngine;
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Autowired
-    public DebeziumListener(Configuration customerConnectorConfiguration, RestaurantService restaurantService) {
-        this.customerConnectorConfiguration = customerConnectorConfiguration;
+    public DebeziumListener(Configuration connectorConfiguration, RestaurantService restaurantService) {
         this.restaurantService = restaurantService;
+        this.connectorConfiguration = connectorConfiguration;
 
         this.debeziumEngine = DebeziumEngine.create(ChangeEventFormat.of(Connect.class))
-                .using(customerConnectorConfiguration.asProperties())
+                .using(connectorConfiguration.asProperties())
                 .notifying(this::handleEvent)
                 .build();
     }
 
     private void handleEvent(RecordChangeEvent<SourceRecord> sourceRecordRecordChangeEvent) {
-        System.out.println("Received event: " + sourceRecordRecordChangeEvent);
+        System.out.println("DB Change Event");
+        System.out.println("Received event: {}" + sourceRecordRecordChangeEvent.record());
     }
 
     @PostConstruct
     private void start() {
-        executorService.submit(() -> {
-            try {
-                debeziumEngine.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        this.executor.execute(debeziumEngine);
     }
 
     @PreDestroy
-    private void stop() {
-        if (debeziumEngine != null) {
-            try {
-                debeziumEngine.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    private void stop() throws IOException {
+        if (this.debeziumEngine != null) {
+            this.debeziumEngine.close();
         }
-        executorService.shutdown();
     }
 }
